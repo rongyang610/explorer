2881af48917a38e62a0a53753b6278d4eb5f28df
JJ Yang <rongyang610@gmail.com>
2019-06-03 21:19:05 -0400

    Starting tx page

--- src/js/reducers/entities/entities_reducer.js
+++ src/js/reducers/entities/entities_reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import btcAddressInfo from './btc_info/btc_address_info_reducer';
+import tx from './tx/tx_reducer';
 
 const entitiesReducer = combineReducers ({
   btcAddressInfo,
+  tx
 });
 
 export default entitiesReducer;
